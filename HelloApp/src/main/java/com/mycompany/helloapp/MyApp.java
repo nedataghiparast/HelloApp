d10fc3250208f98ae5bd22c9195cbd51e91bfac9
Neda Taghiparast <nedataghiparast@gmail.com>
2021-08-09 16:27:17 -0700

    first commit

---
+++ HelloApp/src/main/java/com/mycompany/helloapp/MyApp.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.helloapp;
+
+
+public class MyApp {
+    public static void main(String[] args) {
+        System.out.println("Greeting from CS Corner!!");
+    }
+  
+            
+    
+}
